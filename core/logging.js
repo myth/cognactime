d57546adc367d790fe855aaf6df0260bf17072a9
mythern <myth@overflow.no>
2015-07-02 00:02:28 +0200

    Add handling of logs directory creation.

--- core/logging.js
+++ core/logging.js
@@ -5,6 +5,8 @@
 */
 
 var winston = require('winston')
+var fs = require('fs')
+var path = require('path')
 var config = require('../config.json')
 
 winston.emitErrs = true;
@@ -13,19 +15,26 @@ var logConf = {
   transports: []
 }
 
-if (config.logging.file) {
-  logConf.transports.push(
-    new winston.transports.File({
-      level: config.logging.fileLevel,
-      filename: config.logging.filePath,
-      handleExceptions: false,
-      json: false,
-      maxSize: 5242880,
-      maxFiles: 5,
-      colorize: false
-    })
-  )
+// Helper to create our 'logs' directory if it does not exist
+var ensureExists = function (dir, mask, callback) {
+  if (typeof(mask) == 'function') {
+    callback = mask
+    mask = 0755
+  }
+
+  if (!path.isAbsolute(dir)) {
+    dir = path.resolve(__dirname, '../', dir)
+  }
+
+  try {
+    fs.mkdirSync(dir, mask)
+    callback(null, dir)
+  } catch (e) {
+    if (e.code !== 'EEXIST') callback(e, dir)
+    else callback(null, dir)
+  }
 }
+
 if (config.logging.console) {
   logConf.transports.push(
     new winston.transports.Console({
@@ -35,6 +44,28 @@ if (config.logging.console) {
       colorize: true
     })
   )
+}
+
+if (config.logging.file) {
+  // Create logs directory if it does not exist
+  ensureExists(config.logging.filePath, function (err, dir) {
+    if (!err) {
+      logConf.transports.push(
+        new winston.transports.File({
+          level: config.logging.fileLevel,
+          filename: path.join(dir, 'cognactime.log'),
+          handleExceptions: false,
+          json: false,
+          maxSize: 5242880,
+          maxFiles: 5,
+          colorize: false
+        })
+      )
+    }
+    else {
+      console.error("Failed to create logs directory. File disabled. " + err)
+    }
+  })
 }
 
 var logger = new winston.Logger(logConf)
